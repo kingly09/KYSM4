4652f8a66af633e105c58f3685be0996b66e01f6
kingly <libintm@163.com>
2017-11-06 17:59:42 +0800

    Sm4加密解密Demo

--- KYSM4Demo/KYSM4Demo/ViewController.m
+++ KYSM4Demo/KYSM4Demo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "NSString+KYSM4.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self cbcDemo];
+    [self ECBDemo];
 }
 
 
@@ -25,5 +28,43 @@
     // Dispose of any resources that can be recreated.
 }
 
+/**
+ CBC模式示例
+ */
+-(void)cbcDemo{
+    
+    
+    //密钥
+    NSString *secretKey = @"JeF8U9wHFOMfs2Y8";
+    //初始化向量
+    NSString *iv = @"UISwD9fW6cFh9SNS";
+    // 需要加密的字符串
+    NSString *testString = @"CBC模式示例:测试 国密Sm4算法";
+    
+    NSString *encryptionString = [testString encryptionWithSM4Key:secretKey iv:iv];
+    NSLog(@"加密: %@", encryptionString);
+    
+    NSString *decryptionString = [encryptionString decryptionWithSM4Key:secretKey iv:iv];
+    NSLog(@"解密: %@", decryptionString);
+    
+}
+
+/**
+ ECB模式示例
+ */
+-(void)ECBDemo{
+    
+    //密钥
+    NSString *secretKey = @"JeF8U9wHFOMfs2Y8";
+    // 需要加密的字符串
+    NSString *testString = @"ECB模式示例:测试 国密Sm4算法";
+    
+    NSString *encryptionString = [testString encryptionWithSM4Key:secretKey];
+    NSLog(@"加密: %@", encryptionString);
+    
+    NSString *decryptionString = [encryptionString decryptionWithSM4Key:secretKey];
+    NSLog(@"解密: %@", decryptionString);
+    
+}
 
 @end
