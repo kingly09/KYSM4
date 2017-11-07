14b469da55123d400b6a80f747652d3020dae619
kingly <libintm@163.com>
2017-11-07 10:15:18 +0800

    配置 Fabric

--- KYSM4Demo/KYSM4Demo/AppDelegate.m
+++ KYSM4Demo/KYSM4Demo/AppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
+
 
 @interface AppDelegate ()
 
@@ -17,6 +20,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [Fabric with:@[[Crashlytics class]]];
+
     return YES;
 }
 
