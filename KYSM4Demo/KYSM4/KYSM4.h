bae3b58023e495a3f8025d40a6f0303ecb6b2a12
kingly <libintm@163.com>
2017-11-07 13:01:30 +0800

    添加 .travis.yml

---
+++ KYSM4Demo/KYSM4/KYSM4.h
@@ -0,0 +1,21 @@
+//
+//  KYSM4.h
+//  KYSM4
+//
+//  Created by kingly on 2017/11/7.
+//  Copyright © 2017年 kingly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KYSM4.
+FOUNDATION_EXPORT double KYSM4VersionNumber;
+
+//! Project version string for KYSM4.
+FOUNDATION_EXPORT const unsigned char KYSM4VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KYSM4/PublicHeader.h>
+#import <KYSM4/sm4.h>
+#import <KYSM4/paddingBytes.h>
+#import <KYSM4/NSString+KYSM4.h>
+
